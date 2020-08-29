84fcc06e8401ec64b1f2d524ebfaad2954c562a6
Alexmar <fabioalexmar@gmail.com>
2020-08-28 20:01:30 -0500

    new

--- EjercicioBusquedaBinaria.py
+++ EjercicioBusquedaBinaria.py
@@ -5,25 +5,28 @@ num = 0;
 num2 = lista;
 contador = 0
 while(contador == 0):
-  print("------------------ van ", pasos , " pasos -------------------")
+  
   num2 = int((num2+1)/2)
   varAdivino = num2 + num
 
-  print("1. es menor que ", varAdivino)
-  print("2. es mayor que ", varAdivino)
-  print("3. es su numero ", varAdivino)
-  nom= int(input("----Elije: "))
+  if(varAdivino == numero):
+      print("---------- El numero es: ", varAdivino , "se adivino en ", (pasos - 1) , "pasos -------------")
+      contador=1
+  else:
+    print("------------------ van ", pasos , " pasos -------------------")
+    print("1. es menor que ", varAdivino)
+    print("2. es mayor que ", varAdivino)
 
-  if(nom==1):
-    num = varAdivino - num2
-    
-  if(nom==2):
-    num = varAdivino
+    nom= int(input("----Elije: "))
 
-  if(nom==3):
-    contador=1
+    if(nom==1):
+      num = varAdivino - num2
+      
+    if(nom==2):
+      num = varAdivino
+
     if(varAdivino == numero):
-      print("---------- El numero es: ", varAdivino , "se adivino en ", pasos , "pasos -------------")
-    else:
-      print("Error 404")
+        print("---------- El numero es: ", varAdivino , "se adivino en ", pasos , "pasos -------------")
+        contador=1
+
   pasos = pasos + 1
