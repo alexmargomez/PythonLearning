88d359a5184078ebfd72dff42c4be08e4b85141c
alexmargomez <33881867+alexmargomez@users.noreply.github.com>
2022-06-21 11:42:59 -0500

    Delete easy.py

--- easy.py
+++
@@ -1,9 +0,0 @@
-def prestamo():
-	A = int(input("Ingrese el valor del prestamo:"))
-	N = int(input("A cuanos años: "))
-	I = int(input("Tasa de interes: "))
-	Z = A*(I + 1 )**N
-	return "El valor total a pagar es de %s" %Z
-
-print(prestamo())
-input("-----oprima ara salir------"))
